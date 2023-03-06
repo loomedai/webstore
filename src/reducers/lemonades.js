f92182da98cfdb204fa72e046363bd83d09454b7
loomedai <bbrith@hotmail.com>
2023-03-06 20:01:24 +0100

    added lemons map function

--- src/reducers/lemonades.js
+++ src/reducers/lemonades.js
@@ -1,18 +1,31 @@
 import React, {useState, useEffect} from 'react';
 
 
-const [lemons, setLemons] =
+const [lemons, setLemons] = useState([
+  {title:'LemonSour', body: 'Lorem ipsum', price:'29kr', id: 1},
+  {title:'Sweet n Sour', body: 'Lorem ipsum', price:'20kr', id: 2},
+  {title:'SoulSour', body: 'Lorem ipsum', price:'49kr', id: 3},
+]);
 
-function lemons(){
+function drinks(){
 
   useEffect(() =>{
   fetch("")
   .then((response) => response.json())
   .then((data) => setLemons(data));
-  }, [])
+  }, []);
 
   return(
+    <div className=''>
+
+      {lemons.map((lemon) => (
+        <div className='blog-preview'></div>
+
+      ))}
+    </div>
 
   );
 
 }
+
+export default drinks;
