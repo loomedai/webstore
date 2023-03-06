bfe9a341f70a2bca06b09206666350b9d79acf00
loomedai <bbrith@hotmail.com>
2023-03-06 19:20:04 +0100

    api function start

--- src/reducers/lemonades.js
+++ src/reducers/lemonades.js
@@ -1,34 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 
-class lemons extends React.Component {
-    constructor(props) {
-      super(props);
-  
-      this.state = {
-        loading: true,
-        data: null,
+const [lemons, setLemons] =
 
-      };
-    }
-  
-    async componentDidMount() {
+function lemons(){
 
-    fetch('www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita')
-    .then(Response => Response.json())
-    .then(data => this.setState({ data}));
-     
-    }
-  
-    render() {
-      const { data, error, loading } = this.state;
-  
-      if (!!error) return <h2>{error}</h2>;
-  
-      if (loading) return <h2>Loading...</h2>;
-  
-      return <h2>{data}</h2>;
-    }
-  }
+  useEffect(() =>{
+  fetch("")
+  .then((response) => response.json())
+  .then((data) => setLemons(data));
+  }, [])
 
-export default lemons;
+  return(
+
+  );
+
+}
